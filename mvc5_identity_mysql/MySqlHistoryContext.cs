3eed0ea45c9963c1a5a53056d68db52be333c839
joe62 <yunfei_z@163.com>
2015-10-13 16:40:52 +0800

    增加源代码

---
+++ mvc5_identity_mysql/MySqlHistoryContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity.Migrations.History;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication5
+{
+    public class MySqlHistoryContext:HistoryContext
+    {
+        public MySqlHistoryContext(DbConnection exitingConnection,string defaultSchema)
+            :base(exitingConnection,defaultSchema)
+        { }
+        protected override void OnModelCreating(System.Data.Entity.DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<HistoryRow>().Property(h => h.MigrationId).HasMaxLength(100).IsRequired();
+            modelBuilder.Entity<HistoryRow>().Property(h => h.ContextKey).HasMaxLength(200).IsRequired();
+        }
+    }
+}
---
+++ mvc5_identity_mysql/MySqlInitializer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using WebApplication5.Models;
+
+namespace WebApplication5
+{
+    public class MySqlInitializer:IDatabaseInitializer<ApplicationDbContext>
+    {
+        
+        public void InitializeDatabase(ApplicationDbContext context)
+        {
+            if (!context.Database.Exists())
+            {
+                context.Database.Create();
+            }
+            else
+            {
+                //var strQuery = string.Format("SELECT COUNT(*) FROM　information_schema.tables WHERE table_schem='{0}' AND table_name='__MigrationHistory'",
+
+                //    "[Insert your database schema here - such as 'users']");
+
+                var strQuery = "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = 'itop22' AND table_name = '__MigrationHistory'";
+
+                var migrationHistoryTableExits
+                    = ((IObjectContextAdapter)context).ObjectContext.ExecuteStoreQuery<int>(strQuery);
+
+                if (migrationHistoryTableExits.FirstOrDefault() == 0)
+                {
+                    context.Database.Delete();
+                    context.Database.Create();
+
+                }
+            }
+        }
+    }
+}
